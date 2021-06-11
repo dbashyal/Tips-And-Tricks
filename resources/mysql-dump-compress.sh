f3117000ebb801c991c161ae936221c27d198e6a
Damodar Bashyal <dbashyal@gmail.com>
2021-06-11 10:01:15 +1000

    Create mysql-dump-compress.sh

---
+++ resources/mysql-dump-compress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ `whoami` != 'damu' ]
+  then
+    echo ""
+    echo "You must be user 'damu' to run this."
+        echo ""
+    exit
+fi
+
+sql_dump_date=`date +"%s"`
+sql_dump_name=damu-db-b2b-${sql_dump_date}.sql
+
+mysqldump --hex-blob --skip-triggers --set-gtid-purged=OFF --column-statistics=0 damu_b2b > /home/damu/backups/${sql_dump_name}
+
+sed -i 's/DEFINER=[^*]*\*/\*/g' /home/damu/backups/${sql_dump_name}
+
+tar -cvzf /home/damu/backups/${sql_dump_name} /home/damu/backups/${sql_dump_name}.tar.gz
